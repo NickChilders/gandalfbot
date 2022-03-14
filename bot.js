41efd42b49e4b5a12d2c06713d149934af30534b
nrchilders <74997040+nrchilders@users.noreply.github.com>
2022-03-14 16:51:06 -0400

    Adding quotes

--- bot.js
+++ bot.js
@@ -34,13 +34,16 @@ if (message.substring(0, 1) == '!') {
     args = args.splice(1);
 
     // Array list of replies
-    let replies = [ "This is your realm, and the heart of the greater realm that shall be. The Third Age of the world is ended, and the new age is begun; and it is your task to order its beginning and to preserve what must be preserved. For though much has been saved, much must now pass away; and the power of the Three Rings also is ended. And all the lands that you see, and those that lie round about them, shall be dwellings of Men. For the time comes of the Dominion of Men, and the Elder Kindred shall fade or depart.", 
-                    "I am with you at present, but soon I shall not be. You must settle its affairs yourselves; that is what you have been trained for. Do you not yet understand? My time is over: it is no longer my task to set things to rights, nor to help folk to do so. And as for you, my dear friends, you will need no help. You are grown up now. Grown indeed very high; among the great you are, and I have no longer any fear at all for any of you.", 
+    let replies = [ "For though much has been saved, much must now pass away",
+                    "For the time comes of the Dominion of Men, and the Elder Kindred shall fade or depart.", 
+                    "I am with you at present, but soon I shall not be. You must settle its affairs yourselves", 
+                    "My time is over; it is no longer my task to set things to rights, nor to help folk to do so.",
+                    "You are grown up now. Grown indeed very high; among the great you are, and I have no longer any fear at all for any of you.", 
                     "Only a small part is played in great deeds by any hero.",
                     "I will not say: do not weep; for not all tears are an evil."
     ];
     // Random number from 0 to 3 for the array index
-    let random = Math.floor(Math.random() * 4);
+    let random = Math.floor(Math.random() * 7);
 
     switch(cmd) {
 
@@ -57,6 +60,14 @@ if (message.substring(0, 1) == '!') {
                 to: channelID,
                 message: 'YOU SHALL NOT PASS!',
                 file: 'Balrog.png'
+            });
+        break;
+        case 'Gandalf?':
+            bot.uploadFile({
+                to: channelID,
+                message: 'Gandalf? Oh yes... Thats what thay would call me. Gandalf the Gray. That was my name...*I* am Gandalf the white.',
+                file: 'Gandalf_White.png',
+                message: 'And I come back you now - at the turn of the tide.'
             });
         break;
 
