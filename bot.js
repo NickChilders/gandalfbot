a2cfe3a8978aee56d42df905027ca91faad19f17
nrchilders <74997040+nrchilders@users.noreply.github.com>
2022-03-16 14:03:29 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,8 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
+const prefix = '!Gandalf';
+const preG = '!Google';
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -27,12 +29,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
 
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-
-        args = args.splice(1);
-
+    if (message.content == prefix) {
         // Array list of replies
         let replies = [ "For though much has been saved, much must now pass away",
                         "For the time comes of the Dominion of Men, and the Elder Kindred shall fade or depart.", 
@@ -42,18 +39,38 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                         "Only a small part is played in great deeds by any hero.",
                         "I will not say: do not weep; for not all tears are an evil."
         ];
-        // Random number from 0 to 3 for the array index
+        // Random number from 0 to 7 for the array index
         let random = Math.floor(Math.random() * 7);
-
-        switch(cmd) {
-
-            // !Gandalf
-            case 'Gandalf':
-                bot.sendMessage({
+        bot.sendMessage({
                     to: channelID,
                     message: replies[random]
                 });
-            break;
+        return;
+    }
+    if (message.content.startsWith(preG)){
+        bot.uploadFile({
+            to: channelID,
+            file: 'GandalfGoogle.png'
+        });
+        var NewArgs = message.content.slice(preG.length).split(/ +/);
+
+        var searchTopic = NewArgs.join('+').slice(1)
+        let googleResult = `https://google.com/search?q=${searchTopic}`
+
+        let searchEmbed = new Discord.MessageEmbed()
+        .setColor("#00ff00")
+        .setDescription(`Here's what Google came up with for ${searchTopic}!\n${googleResult}`)
+        message.channel.send(searchEmbed)
+        
+        return;
+    }
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+
+        switch(cmd) {
             // !Balrog
             case 'Balrog':
                 bot.uploadFile({
@@ -79,26 +96,6 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                     to: channelID,
                     file: 'DontFrodo.gif'
                 });
-            break;
-            case 'Gandalf-Google':
-                bot.uploadFile({
-                    to: channelID,
-                    file: 'GandalfGoogle.png'
-                });
-                bot.sendMessage({
-                    to: channelID,
-                    message: "What do you want me to google?"
-                });
-                
-                var NewArgs = message.content.slice(message.length).split(/ +/);
-
-                var searchTopic = NewArgs.join('+').slice(1)
-                let googleResult = `https://google.com/search?q=${searchTopic}`
-
-                let searchEmbed = new Discord.MessageEmbed()
-                .setColor("#00ff00")
-                .setDescription(`Here's what Google came up with for ${searchTopic}!\n${googleResult}`)
-                message.channel.send(searchEmbed)
             break;
             // Just add any case commands if you want to..
         }
