ac4068f3c836748a5690668f655b8061256fb3bb
nrchilders <74997040+nrchilders@users.noreply.github.com>
2022-03-16 13:09:59 -0400

    Attempt for Google Search implementation

--- bot.js
+++ bot.js
@@ -25,64 +25,76 @@ logger.info(bot.username + ' - (' + bot.id + ')');
 
 bot.on('message', function (user, userID, channelID, message, evt) {
 
-// Our bot needs to know if it will execute a command
-// It will listen for messages that will start with `!`
-if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
 
-    args = args.splice(1);
+        args = args.splice(1);
 
-    // Array list of replies
-    let replies = [ "For though much has been saved, much must now pass away",
-                    "For the time comes of the Dominion of Men, and the Elder Kindred shall fade or depart.", 
-                    "I am with you at present, but soon I shall not be. You must settle its affairs yourselves", 
-                    "My time is over; it is no longer my task to set things to rights, nor to help folk to do so.",
-                    "You are grown up now. Grown indeed very high; among the great you are, and I have no longer any fear at all for any of you.", 
-                    "Only a small part is played in great deeds by any hero.",
-                    "I will not say: do not weep; for not all tears are an evil."
-    ];
-    // Random number from 0 to 3 for the array index
-    let random = Math.floor(Math.random() * 7);
+        // Array list of replies
+        let replies = [ "For though much has been saved, much must now pass away",
+                        "For the time comes of the Dominion of Men, and the Elder Kindred shall fade or depart.", 
+                        "I am with you at present, but soon I shall not be. You must settle its affairs yourselves", 
+                        "My time is over; it is no longer my task to set things to rights, nor to help folk to do so.",
+                        "You are grown up now. Grown indeed very high; among the great you are, and I have no longer any fear at all for any of you.", 
+                        "Only a small part is played in great deeds by any hero.",
+                        "I will not say: do not weep; for not all tears are an evil."
+        ];
+        // Random number from 0 to 3 for the array index
+        let random = Math.floor(Math.random() * 7);
 
-    switch(cmd) {
+        switch(cmd) {
 
-        // !Gandalf
-        case 'Gandalf':
-            bot.sendMessage({
-                to: channelID,
-                message: replies[random]
-            });
-        break;
-        // !Balrog
-        case 'Balrog':
-            bot.uploadFile({
-                to: channelID,
-                message: 'YOU SHALL NOT PASS!',
-                file: 'Balrog.png'
-            });
-        break;
-        //!Gandalf?
-        case 'Gandalf?':
-            bot.sendMessage({
-                to: channelID,
-                message: "Gandalf? Oh yes... That's what they would call me. Gandalf the Gray. That was my name...*I* am Gandalf the white."
-            });
-            bot.uploadFile({
-                to: channelID,
-                file: 'Gandalf_White.png',
-                message: 'And I come back you now - at the turn of the tide.'
-            });
+            // !Gandalf
+            case 'Gandalf':
+                bot.sendMessage({
+                    to: channelID,
+                    message: replies[random]
+                });
             break;
-        // !Take-it
-        case 'Take-it':
-            bot.uploadFile({
-                to: channelID,
-                file: 'DontFrodo.gif'
-            });
-        break;
+            // !Balrog
+            case 'Balrog':
+                bot.uploadFile({
+                    to: channelID,
+                    message: 'YOU SHALL NOT PASS!',
+                    file: 'Balrog.png'
+                });
+            break;
+            //!Gandalf?
+            case 'Gandalf?':
+                bot.sendMessage({
+                    to: channelID,
+                    message: "Gandalf? Oh yes... That's what they would call me. Gandalf the Gray. That was my name...*I* am Gandalf the white. And I come back you now - at the turn of the tide."
+                });
+                bot.uploadFile({
+                    to: channelID,
+                    file: 'Gandalf_White.png',
+                });
+            break;
+            // !Take-it
+            case 'Take-it':
+                bot.uploadFile({
+                    to: channelID,
+                    file: 'DontFrodo.gif'
+                });
+            break;
+            case 'Gandalf-Goole':
+                bot.uploadFile({
+                    to: channelID,
+                    file: 'GandalfGoogle.png'
+                });
+                const args = message.content.slice(cmd.length).split(/ +/);
+                const searchTopic = args.join('+').slice(1)
+                let googleResult = `https://google.com/search?q=${searchTopic}`
+                let searchEmbed = new Discord.MessageEmbed()
+                .setColor("#00ff00")
+                .setDescription(`Here's what Google came up with for ${searchTopic}!\n${googleResult}`)
+                message.channel.send(searchEmbed)
+            break;
+            // Just add any case commands if you want to..
+        }
+    }
 
-        // Just add any case commands if you want to..
-     }
- }
 });
