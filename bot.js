5b7f0dc56f989bf80283dba59bde1ef1bf2f805f
nrchilders <74997040+nrchilders@users.noreply.github.com>
2022-03-16 16:10:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -91,7 +91,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                const searchEmbed = new MessageEmbed()
                .setColor("#00ff00")
                .setDescription(`Here's what Google came up with for ${searchTopic}!\n${googleResult}`);
-               bot.send({ to: channelID, embeds: [searchEmbed]});
+               bot.sendMessage({ to: channelID, embeds: [searchEmbed]});
         }
         
     }
